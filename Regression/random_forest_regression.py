06ef7a6dc4909b45b913f934100319d3da0f0ff1
Ankur800 <ankurrai800@gmail.com>
2020-09-27 16:57:37 +0530

    Done with Regression!

---
+++ Regression/random_forest_regression.py
@@ -0,0 +1,27 @@
+# IMPORTING LIBRARIES
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# IMPORTING DATASET
+dataset = pd.read_csv("/home/ankur/Udemy ML/Machine Learning A-Z (Codes and Datasets)/Part 2 - Regression/Section 9 - Random Forest Regression/Python/Position_Salaries.csv")
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+# TRAINING THE DECISION TREE REGRESSION MODEL ON THE WHOLE DATASET
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators=10, random_state=0)
+regressor.fit(X, y)
+
+# PREDICTING A NEW RESULT
+print(regressor.predict([[6.5]]))
+
+# VISUALISING THE DECISION TREE REGRESSION RESULTS
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
+plt.title('Truth or Bluff (Support Vector Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
